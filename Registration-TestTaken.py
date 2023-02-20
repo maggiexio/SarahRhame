f1b64d1f7cd68c7889ef431508bb746cedfecbdc
maggiexio <53416822+maggiexio@users.noreply.github.com>
2023-02-20 18:32:24 -0500

    Update Registration-TestTaken.py

--- Registration-TestTaken.py
+++ Registration-TestTaken.py
@@ -125,19 +125,19 @@ default_mon=mon_choice.index('All')
 
 mod_select = st.sidebar.selectbox('Select mode:', mod_choice, index=default_mod)
 if mod_select != "All":
-  df_1=df_1.query("Mode==@mod_choice")
+  df_1=df_1.query("Mode==@mod_select")
 reg_select = st.sidebar.selectbox('Select region:', reg_choice, index=default_reg)  
 if reg_select != "All":
-  df_1=df_1.query("Region==@reg_choice")
+  df_1=df_1.query("Region==@reg_select")
 cty_select = st.sidebar.selectbox('Select country:', cty_choice, index=default_cty)
 if cty_choice != "All":
-  df_1=df_1.query("Country==@cty_choice")
+  df_1=df_1.query("Country==@cty_select")
 yy_select = st.sidebar.selectbox('Select year:', yy_choice, index=default_yy)
 if yy_select != "All":
-  df_1=df_1.query("Year==@yy_choice")
+  df_1=df_1.query("Year==@yy_select")
 mon_select = st.sidebar.selectbox('Select month:', mon_choice, index=default_mon)
 if mon_select != "All":
-  df_1=df_1.query("Month==@mon_choice")
+  df_1=df_1.query("Month==@mon_select")
 
 
 # figures display, based on file df_1 which is data after filters apply
