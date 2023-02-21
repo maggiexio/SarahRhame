e8a2daa5095802b742da7fca303da3ccca1ab194
maggiexio <53416822+maggiexio@users.noreply.github.com>
2023-02-21 18:49:42 -0500

    Update Registration-TestTaken.py

--- Registration-TestTaken.py
+++ Registration-TestTaken.py
@@ -24,8 +24,8 @@ with col11:
   subj_1="-- Fiscal year 2020 to 2023"
   st.markdown(f'<h2 style="text-align: center;color: green;">{subj_1}</h2>',unsafe_allow_html=True) 
   st.markdown ("By: Sarah Rhame")
-  st.markdown("Original data set include 6694 registraion and 7105 test-taken records worldwide for fiscal years 2021 to 2023 (2020/10 to 2023/2). 
-              The following two type of records were removed in the clean data set: 1. 464 records with no volume; 2. 322 records in 2023 March to September.
+  st.markdown("Original data set include 6694 registraion and 7105 test-taken records worldwide for fiscal years 2021 to 2023 (2020/10 to 2023/2). \ 
+              The following two type of records were removed in the clean data set: 1. 464 records with no volume; 2. 322 records in 2023 March to September. \
               Therefore, there are 13013 records in the clean data set. ")
    
 with col12:
