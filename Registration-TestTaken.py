4021b75978a9112fdb07e8fe4f67d1597206460c
maggiexio <53416822+maggiexio@users.noreply.github.com>
2023-02-20 21:59:51 -0500

    Update Registration-TestTaken.py

--- Registration-TestTaken.py
+++ Registration-TestTaken.py
@@ -189,7 +189,7 @@ with col11:
     fig_ani1=px.bar(df_1, x='YY_Mon', y='N', animation_frame='Reg_Cty', color='Mode')
     fig_ani1.update_layout(transition = {'duration': 30000})
     st.plotly_chart(fig_ani1,  use_container_width=True, height=600)
-    fig_ani2=px.scatter(df_1, y='N', x='YY_Mon', animation_frame=('Region', 'Country'), color='Mode', size='N_scale', size_max=60)
+    fig_ani2=px.scatter(df_1, y='N', x='YY_Mon', animation_frame='Reg_Cty', color='Mode', size='N_scale', size_max=60)
     fig_ani2.update_layout(transition = {'duration': 30000})
     st.plotly_chart(fig_ani2,  use_container_width=True, height=600)   
   with st.expander("Pie Charts:    check volume distribution for each region/country/year/month"):    
