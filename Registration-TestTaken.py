b8902eef5bf931f672f8ec6e882214a98cce3cb1
maggiexio <53416822+maggiexio@users.noreply.github.com>
2023-02-20 20:15:51 -0500

    Update Registration-TestTaken.py

--- Registration-TestTaken.py
+++ Registration-TestTaken.py
@@ -160,8 +160,8 @@ with col11:
   with st.expander("Histogram: distributions of monthly registration/TestTaken volume for each region/country/year/month "):    
      #figure_hist1 = px.histogram(df_1, x='YY_Mon', y='N', animation_frame='Region', color='Country', 
      #                      facet_col='Mode', marginal='box')
-     figure_hist1 = px.histogram(df_1, x='YY_Mon',  y='N', animation_frame='Region', 
-                           facet_col='Mode', marginal='box')
+     figure_hist1 = px.histogram(df_1, x='YY_Mon',  y='N', animation_frame='Region', color='Country', 
+                            marginal='box')
      st.plotly_chart(figure_hist1,  use_container_width=True, height=800)
   
   df_ori['Month_N'] = [strptime(str(x), '%b').tm_mon for x in df_ori['Month'].str.slice(0, 3)]
