49d8cf6aa35071b26f4df59e9be3f8804790a4e3
maggiexio <53416822+maggiexio@users.noreply.github.com>
2023-02-22 11:54:32 -0500

    Update Registration-TestTaken.py

--- Registration-TestTaken.py
+++ Registration-TestTaken.py
@@ -219,7 +219,7 @@ with col11:
     fig_ani2.update_layout(transition = {'duration': 10000})
     st.plotly_chart(fig_ani2,  use_container_width=True, height=600)   
   with st.expander("Pie Charts: check volume distribution for each region/country/year/month. Top plot is for registration volume and the plot in middle is for test-taken volume. The plot at bottom is for both types of volumes."):    
-    fig_31=px.sunburst(df_1_res, color='N', path=['Region', 'Country', 'Year', 'Month'], facet_col='Mode', color_continuous_scale='Inferno')
+    fig_31=px.sunburst(df_1_res, color='N', path=['Region', 'Country', 'Year', 'Month'], color_continuous_scale='Inferno')
     #st.plotly_chart(fig_31,   use_container_width=True, height=600)
     fig_32=px.sunburst(df_1_taken, color='N',  path=['Region', 'Country', 'Year', 'Month'], color_continuous_scale='Inferno')
     #st.plotly_chart(fig_32,   use_container_width=True, height=600) 
